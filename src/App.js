3d628fedcee6041a2b3ea2f9462d8bc997f607c0
Wis <wiskojo@yahoo.com>
2017-10-21 09:05:43 -0700

    Implemented updateUserLocation and removeUserLocation functions.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 // import UserCircle from './components/UserCircle';
-import GChart from './components/GChart';
+// import GChart from './components/GChart';
 import * as firebase from "firebase";
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 import graph from 'fb-react-sdk';
+import Geohash from 'latlon-geohash'
+
 
 var config = {
   apiKey: "AIzaSyBc9VGjprDNigBP-2TUAai1wpfmH9cqhBU",
@@ -39,26 +41,66 @@ class App extends Component {
 
 constructor(props){
   super(props);
-  this.state = {loggedIn: false};
+  this.state = {loggedIn: false, prevHash: null};
   this.login = this.login.bind(this);
 }
 
+updateUserLocation(user)
+{
+  if("geolocation" in navigator)
+  {
+    navigator.geolocation.getCurrentPosition((position) => {
+      var geohash = Geohash.encode(position.coords.latitude,
+                                   position.coords.longitude,
+                                   this.precision);
 
- login(){
+      if(geohash != this.state.prevHash)
+      {
+        // fan out geohash change
+        var ref = firebase.database().ref();
+        var fanoutObject = {};
+
+        if(this.state.prevHash) fanoutObject['/geohashes/' + this.state.prevHash + '/' + user.uid] = null;
+        fanoutObject['/geohashes/' + geohash + '/' + user.uid] = { name: user.displayName, profile_pic: user.photoURL };
+        fanoutObject['/users/' + user.uid + '/geohash'] = geohash;
+
+        // Store geohash
+        this.setState({ prevHash: geohash });
+
+        // atomic update
+        ref.update(fanoutObject);
+      }
+    }, (err) => {
+      // error handling here
+      console.log("Geolocation error");
+    })
+  }
+}
+
+removeUserLocation(user)
+{
+  if(!this.state.prevHash)
+    firebase.database().ref('/geohashes/' + this.state.prevHash + '/' + user.uid).remove();
+}
+
+ login() {
    var provider = new firebase.auth.FacebookAuthProvider();
-provider.addScope('user_friends');
+   provider.addScope('user_friends');
    firebase.auth().signInWithPopup(provider).then((result)=> {
-  // This gives you a Facebook Access Token.
-  var token = result.credential.accessToken;
-  // The signed-in user info.
-  var user = result.user;
+   // This gives you a Facebook Access Token.
+   var token = result.credential.accessToken;
+   // The signed-in user info.
+   var user = result.user;
 
-  console.log(result);
+   console.log(result);
+
+   this.updateUserLocation(result.user);
 
   this.setState({
     loggedIn: true
   });
   var uid = user.uid;
+
 graph.setAccessToken(token);
 graph.get(`/me/friends`, function(err, res) {
         // returns the post id
@@ -73,7 +115,7 @@ graph.get(`/me/friends`, function(err, res) {
 
  renderGraph(){
    if(this.state.loggedIn){
-     return <GChart />
+    //  return <GChart />
    }
    else {
     return <RaisedButton label="Log In" primary={true} onClick={this.login} />
